46901bd9f8fe57b412bba59c8b63a1fb7c6581a2
111202214613@mhs.dinusnac.id <111202214613@mhs.dinus.ac.id>
2024-02-17 00:11:15 +0700

    Kumpulan seluruh progress belajar tailwindcss

---
+++ belajar-tailwind 2/tailwind.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      spacing: {
+        '13': '13.5rem',
+        '144': '36rem',
+      },
+      fontFamily: {
+        roboto: ['Roboto'],
+      }
+  },
+},
+  plugins: [],
+}
+
